a6ea74ccfa8363ead68645bb6623cd97f0bd6306
brandonnjug-oss <brandon.njug@gmail.com>
2025-08-23 09:23:23 +0300

    Update (TEST)gas_detector.ino

--- (TEST)gas_detector.ino
+++ (TEST)gas_detector.ino
@@ -1,29 +1,39 @@
 // Pin assignments
-const int mq2Pin   = A0;   // MQ-2 analog output
-const int ledPin   = 12;   // LED
-const int buzzerPin = 13;  // Active buzzer
+const int mq2Pin   = A0;    // MQ-2 analog output
+const int ledPin   = 12;    // LED
+const int buzzerPin = 13;   // Active buzzer
 
 // Gas detection threshold (adjust after testing)
-int threshold = 250;  // analog value, range 0–1023
-unsigned long levelStart = 0;
+int threshold = 250;    // analog value, range 0–1023
+
+unsigned long lastAlarmToggle = 0;    //tracks the last time the buzzer and LED were toggled
+
+unsigned long lastReport = 0;   //tracks the last time a value of the gas reading was sent to the computer
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);  // for monitoring readings
 }
 
 void loop() {
-  int gasValue = analogRead(mq2Pin);  // Read gas level
+  int gasValue = analogRead(mq2Pin);  // read gas level
 
   if (gasValue > threshold) {
-    if (millis() - levelStart >= 200) { //causes screen refresh every 200ms
-      digitalWrite(ledPin, !digitalRead(ledPin));   // LED FLASH
-      digitalWrite(buzzerPin, !digitalRead(buzzerPin)); // Buzzer FLASH
-      levelStart = millis();
-      gasValue = analogRead(mq2Pin);
+    if (millis() - lastAlarmToggle >= 200) {              // causes toggle every 200ms
+      digitalWrite(ledPin, !digitalRead(ledPin));         // LED toggle
+      digitalWrite(buzzerPin, !digitalRead(buzzerPin));   // Buzzer toggle
+      lastAlarmToggle = millis();                         // sets the lastAlarmToggle to the program run time
+      gasValue = analogRead(mq2Pin);                      // read gas level
     }
   } else {
     digitalWrite(ledPin, LOW);    // LED OFF
     digitalWrite(buzzerPin, LOW);  // Buzzer OFF
+  }
+
+  if (millis() - lastReport >= 200) {   // causes the reading every 200ms
+    Serial.print("Gas value:");          
+    Serial.println(gasValue);            // these 2 lines send the gas value eto the computer
+    lastReport = millis();               // sets the last report time to the program run time
   }
 }
